4a9d41d20eae59a2336fda51d21ca1d7c140c208
Hashan <hemachandra.hashan@gmail.com>
2024-03-09 08:44:12 +0530

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+import MaxWIdthWrapper from '@/components/MaxWIdthWrapper'
 
 export default function Home() {
-  return (
-    <div></div>
-  );
+    return (
+        <MaxWIdthWrapper className="mb-12 mt-28 sm:mb-40 flex flex-col items-center justify-center text-center">
+            <div className="mx-auto mb-4 max-w-fit items-center justify-center space-x-2  overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur hover:border-gray-300 hover:bg-white/50">
+                <p className="text-sm font-semibold text-gray-700">
+                    Quiz is now public!
+                </p>
+            </div>
+            <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+                Chat with your{' '}
+                <span className="text-purple-600">documents</span> in seconds.
+            </h1>
+            <p className='max-w-prose mt-5 text-zinc-700 sm:text-lg'>Quiz allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away.</p>
+        </MaxWIdthWrapper>
+    )
 }
