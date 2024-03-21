2bebbeebc607866555cefa076e6390cbadc9de80
Hashan <hemachandra.hashan@gmail.com>
2024-03-22 05:06:03 +0530

    Steps continued

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <MaxWIdthWrapper className="mb-12 mt-28 sm:mb-40 flex flex-col items-center justify-center text-center">
                 <div className="mx-auto mb-4 max-w-fit items-center justify-center space-x-2  overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur hover:border-gray-300 hover:bg-white/50">
                     <p className="text-sm font-semibold text-gray-700">
-                        Quiz is now public!
+                        Quizz is now public!
                     </p>
                 </div>
                 <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -26,9 +26,9 @@ export default function Home() {
                     seconds.
                 </h1>
                 <p className="max-w-prose mt-5 text-zinc-700 sm:text-lg">
-                    Quiz allows you to have conversations with any PDF document.
-                    Simply upload your file and start asking questions right
-                    away.
+                    Quizz allows you to have conversations with any PDF
+                    document. Simply upload your file and start asking questions
+                    right away.
                 </p>
                 <Link
                     className={buttonVariants({
@@ -91,6 +91,29 @@ export default function Home() {
             </div>
 
             {/* Feature section */}
+            <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+                <div className="mb-12 px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl sm:text-center">
+                        <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+                            Start Chatting in Minutes
+                        </h2>
+                        <p className="mt-4 text-lg text-gray-600">
+                            Chatting to your PDF files has never been easier
+                            than with Quizz
+                        </p>
+                    </div>
+                </div>
+
+                {/* steps */}
+                <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+                    <li className="md:flex-1">
+                        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                            <span className='text-sm font-medium text-purple-600'>Step 1</span>
+                            <span className='text-xl font-semibold'>Create an account</span>
+                        </div>
+                    </li>
+                </ol>
+            </div>
         </>
     )
 }
