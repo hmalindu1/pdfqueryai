9a6d9eec630b3ec57c6b6127037517f4750a6f8b
Hashan <hemachandra.hashan@gmail.com>
2024-03-24 19:04:13 +0530

    Navbar cont...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'></nav>
   )
 }
 
