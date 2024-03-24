c9ab321c66f3dde15456f97c39b4fe741e234fd8
Hashan <hemachandra.hashan@gmail.com>
2024-03-25 05:03:17 +0530

    Navbar cont...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import MaxWIdthWrapper from './MaxWIdthWrapper'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
 
 const Navbar = () => {
-  return (
-    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'></nav>
-  )
+    return (
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWIdthWrapper>
+                <div className="flex h-14 items-center justify-between border-b boreder-zinc-200">
+                    <Link href="/" className="flex z-40 font-semibold">
+                        <span>Quizz</span>
+                    </Link>
+
+                    {/* todoo: add mobile navbar */}
+
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link
+                                href={'/pricing'}
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm'
+                                })}
+                            >
+                                Pricing
+                            </Link>
+                        </>
+                    </div>
+                </div>
+            </MaxWIdthWrapper>
+        </nav>
+    )
 }
 
 export default Navbar
