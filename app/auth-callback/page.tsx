f0b53d8c6ac2dcd82934bbd438644a1e6edec5bf
hashan <hemachandra.hashan@gmail.com>
2024-03-29 14:22:47 +0530

    kinde auth started

---
+++ app/auth-callback/page.tsx
@@ -0,0 +1,13 @@
+import { useRouter, useSearchParams } from 'next/navigation'
+import React from 'react'
+
+const AuthCallBack = () => {
+    const router = useRouter() 
+    const searchParamas = useSearchParams()
+    const origin = searchParamas.get('origin')
+  return (
+    <div>AuthCallBack</div>
+  )
+}
+
+export default AuthCallBack
---
+++ components/Providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React, { useState } from 'react'
+import { QueryClient } from '@tanstack/react-query'
+
+const Providers = () => {
+    const [queryClient] = useState(() => new QueryClient())
+    const [trpcClient] = useState(() => {})
+  return (
+    <div>Providers</div>
+  )
+}
+
+export default Providers
+
