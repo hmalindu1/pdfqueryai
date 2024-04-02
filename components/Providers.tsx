1d2f9d9ea3cb29f1c586758ebf0cd8c287703843
hashan <hemachandra.hashan@gmail.com>
2024-04-02 17:01:57 +0530

    trpc continued..

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,14 +2,23 @@
 
 import React, { useState } from 'react'
 import { QueryClient } from '@tanstack/react-query'
+import { trpc } from '@/app/_trpc/client'
+import { httpBatchLink } from '@trpc/client'
 
-const Providers = () => {
+const Providers = ({ children }: { children: React.ReactNode }) => {
     const [queryClient] = useState(() => new QueryClient())
-    const [trpcClient] = useState(() => {})
-  return (
-    <div>Providers</div>
-  )
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: 'http://localhost:3000/api/trpc'
+                })
+            ]
+        })
+    )
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>{children}</trpc.Provider>
+    )
 }
 
 export default Providers
-
