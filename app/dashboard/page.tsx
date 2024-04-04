6dc5dcbf942d8e4c2ef94cb3db3fc9634911e97a
hashan <hemachandra.hashan@gmail.com>
2024-04-04 16:27:33 +0530

    js docs added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,11 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
+/**
+ * Generates the Dashboard component asynchronously.
+ *
+ * @return {JSX.Element} The JSX element displaying the user's email within a div.
+ */
 const Dashboard = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
