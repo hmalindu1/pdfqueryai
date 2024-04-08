0f9483381196bb123e2c144ff807d72e9514d46f
hashan <hemachandra.hashan@gmail.com>
2024-04-08 16:54:51 +0530

    updated js docs

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,12 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/trpc';
 
+/**
+ * A function that handles the request by fetching data from a specified endpoint using the given request object.
+ *
+ * @param {Request} req - the request object containing information about the request
+ * @return {Promise<any>} a promise that resolves to the fetched data
+ */
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
