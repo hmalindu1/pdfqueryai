5ba6de416bbccda6b04255053fd9822e5da55225
Hashan <hemachandra.hashan@gmail.com>
2024-04-16 22:27:25 +0530

    jsdocs added

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -2,6 +2,9 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { trpc } from '../_trpc/client'
 
+/**
+ * Function for handling the authentication callback.
+ */
 const AuthCallBack = () => {
     const router = useRouter()
     const searchParamas = useSearchParams()
