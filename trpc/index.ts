c342e64ba9c737239a351f89e72d8925a9747d86
hashan <hemachandra.hashan@gmail.com>
2024-04-17 14:27:25 +0530

    Got the user from Kind server session and checking it with TRPC

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,7 +1,18 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { publicProcedure, router } from './trpc'
+import { TRPCError } from '@trpc/server'
 export const appRouter = router({
-    test: publicProcedure.query(() => {
-        return 'hello'
-    }),     
+    authCallback: publicProcedure.mutation(async () => {
+        const {getUser} =  getKindeServerSession()
+        const user = await getUser()
+
+        if (!user?.id || !user.email) {
+            throw new TRPCError({ code: 'UNAUTHORIZED' })
+        }
+
+        // check if the user is in the database
+
+        return { success: true }
+    })
 })
 export type AppRouter = typeof appRouter
