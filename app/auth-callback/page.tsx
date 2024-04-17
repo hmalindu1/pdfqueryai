0547c027eaed8a40df6a491d9524f90b34ad905c
hashan <hemachandra.hashan@gmail.com>
2024-04-17 14:28:00 +0530

    Checking user existance with tRPC

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -2,14 +2,17 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { trpc } from '../_trpc/client'
 
-/**
- * Function for handling the authentication callback.
- */
 const AuthCallBack = () => {
     const router = useRouter()
     const searchParamas = useSearchParams()
     const origin = searchParamas.get('origin')
-    const {data} = trpc.test.useQuery()
+    const { data } = trpc.authCallback.useMutation({
+        onSuccess: ({ success }) => {
+            if (success) {
+                router.push(origin ? `/${origin}` : '/dashboard')
+            }
+        }
+    })
 }
 
 export default AuthCallBack
