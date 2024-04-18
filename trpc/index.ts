2a2a0172202b2f7e8515cb760324e0c20d72f787
hashan <hemachandra.hashan@gmail.com>
2024-04-18 17:43:31 +0530

    if there is no user create user inside the db

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,9 +1,10 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { publicProcedure, router } from './trpc'
 import { TRPCError } from '@trpc/server'
+import { db } from '@/db'
 export const appRouter = router({
     authCallback: publicProcedure.mutation(async () => {
-        const {getUser} =  getKindeServerSession()
+        const { getUser } = getKindeServerSession()
         const user = await getUser()
 
         if (!user?.id || !user.email) {
@@ -11,6 +12,11 @@ export const appRouter = router({
         }
 
         // check if the user is in the database
+        const dbUser = await db.user.findFirst({ where: { id: user.id } })
+
+        if (!dbUser) {
+            await db.user.create({ data: { id: user.id, email: user.email } })
+        }
 
         return { success: true }
     })
