2af265a4405604c4077b5dcf51febb02d012675e
hashan <hemachandra.hashan@gmail.com>
2024-04-18 17:44:18 +0530

    User redirection added

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,18 +1,39 @@
+'use client'
+
 import { useRouter, useSearchParams } from 'next/navigation'
-import React from 'react'
 import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
 
 const AuthCallBack = () => {
     const router = useRouter()
+
     const searchParamas = useSearchParams()
     const origin = searchParamas.get('origin')
-    const { data } = trpc.authCallback.useMutation({
+
+    trpc.authCallback.useMutation({
         onSuccess: ({ success }) => {
             if (success) {
                 router.push(origin ? `/${origin}` : '/dashboard')
             }
-        }
+        },
+        onError: (error) => {
+            if (error.data?.code === 'UNAUTHORIZED') {
+                router.push('/sign-in')
+            }
+        },
+        retry: true,
+        retryDelay: 500
     })
+
+    return (
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+                <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+                <p>You will be redirected automatically</p>
+            </div>
+        </div>
+    )
 }
 
 export default AuthCallBack
