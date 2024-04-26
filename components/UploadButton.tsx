bed2ad2e74794aaf09146d6617c536d5e7ec6f90
Hashan <hemachandra.hashan@gmail.com>
2024-04-26 05:46:34 +0530

    Upload Button Continued

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
   return (
     <div>UploadButton</div>
   )
