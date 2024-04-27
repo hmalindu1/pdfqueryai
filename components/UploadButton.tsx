fa18dbc2e335f11e9ab82354a71a38ad6c0cd2d9
Hashan <hemachandra.hashan@gmail.com>
2024-04-27 18:22:56 +0530

    UploadButton component continue

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,10 +1,26 @@
+'use client'
 import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
 
 const UploadButton = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  return (
-    <div>UploadButton</div>
-  )
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    return (
+        <Dialog
+            open={isOpen}
+            onOpenChange={(v) => {
+                if (!v) {
+                    setIsOpen(v)
+                }
+            }}
+        >
+            <DialogTrigger asChild onClick={() => setIsOpen(true)}>
+                <Button>Upload PDF</Button>
+            </DialogTrigger>
+
+            <DialogContent>Test Content</DialogContent>
+        </Dialog>
+    )
 }
 
 export default UploadButton
