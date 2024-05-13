9458827e3155816500b1b4a27548fdee2addff1b
Hashan <hemachandra.hashan@gmail.com>
2024-05-13 23:14:38 +0530

    Button to navigate down

--- components/PdfRenderer.tsx
+++ components/PdfRenderer.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { Loader2 } from 'lucide-react'
+import { ChevronDown, Loader2 } from 'lucide-react'
 import React from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 import { useToast } from './ui/use-toast'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
-import { useResizeDetector } from "react-resize-detector";
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button'
 
 interface PdfRendererProps {
     url: string
@@ -20,28 +21,33 @@ interface PdfRendererProps {
  * @return {JSX.Element} The rendered PDF document component
  */
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+    const { toast } = useToast()
 
-    const {toast} = useToast()
+    const { width, ref } = useResizeDetector()
 
-    const {width, ref} = useResizeDetector()
-    
     return (
         <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-                <div className="flex items-center gap-1.5">Top Bar</div>
+                <div className="flex items-center gap-1.5">
+                    <Button variant="ghost" aria-label="Previous Page">
+                        <ChevronDown className="h-4 w-4" />
+                    </Button>
+                </div>
             </div>
 
             <div className="flex-1 w-full max-h-screen">
                 <div ref={ref}>
                     <Document
-                        loading={<div className='flex justify-center'>
-                            <Loader2 className='my-24 h-6 w-6 animate-spin'/>
-                        </div>}
+                        loading={
+                            <div className="flex justify-center">
+                                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                            </div>
+                        }
                         onLoadError={() => {
                             toast({
                                 title: 'Error loading PDF',
                                 description: 'Please try again later',
-                                variant: 'destructive',
+                                variant: 'destructive'
                             })
                         }}
                         file={url}
