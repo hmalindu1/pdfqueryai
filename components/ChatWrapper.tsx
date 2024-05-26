a6a1b45a13bccb262390e547309f14d50c018c06
Hashan <hemachandra.hashan@gmail.com>
2024-05-26 20:30:38 +0530

    Chat wrapper updated

--- components/ChatWrapper.tsx
+++ components/ChatWrapper.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ChatWrapper = () => {
   return (
-    <div>ChatWrapper</div>
+    <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200'>
+      <div className='flex-1 justify-between flex flex-col mb-28'>
+        
+      </div>
+    </div>
   )
 }
 
