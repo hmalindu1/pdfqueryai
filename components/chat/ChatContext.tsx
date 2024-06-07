f22f1ae6dc6a6de1b7b9aa861796349eb463425c
Hashan <hemachandra.hashan@gmail.com>
2024-06-07 06:49:10 +0530

    bug fixed

--- components/chat/ChatContext.tsx
+++ components/chat/ChatContext.tsx
@@ -31,7 +31,7 @@ export const ChatContextProvider = ({
     const { toast } = useToast()
 
     const { mutate: sendMessage } = useMutation({
-        mutationFn: async ({message} : {message: string}) => {
+        mutationFn: async ({ message }: { message: string }) => {
             const response = await fetch('/api/message', {
                 method: 'POST',
                 body: JSON.stringify({
@@ -56,12 +56,16 @@ export const ChatContextProvider = ({
         sendMessage({ message })
     }
 
-    return (<div>
-        <ChatContext.Provider value={{
-            addMessage, message, handleInputChange, isLoading
-
-        }}>
+    return (
+        <ChatContext.Provider
+            value={{
+                addMessage,
+                message,
+                handleInputChange,
+                isLoading
+            }}
+        >
             {children}
         </ChatContext.Provider>
-    </div>)
+    )
 }
