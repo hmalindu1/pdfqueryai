1ca441afa21629057074cbf714d7e74c4c0184f5
hashan <hemachandra.hashan@gmail.com>
2024-07-21 14:52:34 +0530

    absolute url function added

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export const cn = (...input: ClassValue[]) => {
     return twMerge(clsx(input))
 }
+
+
+export function absoluteUrl(path: string) {
+    if (typeof window !== 'undefined') return path
+    if (process.env.AMPLIFY_URL)
+        return `https://${process.env.AMPLIFY_URL}${path}`
+    return `http://localhost:${process.env.PORT ?? 3000}${path}`
+}
