ad1185bc3112480abfc91d5c733045712ec7e866
hashan <hemachandra.hashan@gmail.com>
2024-07-22 06:42:44 +0530

    dummy

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Merges and concatenates class values using the `clsx` and `twMerge` functions.
+ *
+ * @param {ClassValue[]} input - An array of class values to be merged and concatenated.
+ * @return {ClassValue} - The merged and concatenated class value.
+ */
 export const cn = (...input: ClassValue[]) => {
     return twMerge(clsx(input))
 }
