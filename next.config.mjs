b0b8a04df8c241f81f82ee2c920a7f25ddb2d49d
hashan <hemachandra.hashan@gmail.com>
2024-08-01 08:38:31 +0530

    configurations added

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'gravatar.com',
+                port: '',
+                pathname: '/avatar/**'
+            }
+        ]
+    },
+
+    async redirects() {
+        return [
+            {
+                source: '/sign-in',
+                destination: '/api/auth/login',
+                permanent: true
+            },
+            {
+                source: '/sign-up',
+                destination: '/api/auth/register',
+                permanent: true
+            }
+        ]
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
